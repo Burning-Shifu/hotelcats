d1640f6cd79feba7c57496b4a2b0ed79c29c005b
Shifu <kris.deniso@mail.ru>
2020-11-18 18:52:21 +0300

    done with reviews

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,20 @@
 let swiper = new Swiper('.rooms__slider', {
-  loop: true,
+  // loop: true,
+  pagination: {
+    el: '.slider__pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.slider__arrow--next',
+    prevEl: '.slider__arrow--prev',
+  },
+});
+
+new Swiper('.reviews__slider', {
+  slideClass: 'reviews__slide',
+  freeMode: true,
+  slidesPerView: 3,
+  spaceBetween: 30,
   pagination: {
     el: '.slider__pagination',
     clickable: true,
