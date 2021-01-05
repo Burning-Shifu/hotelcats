421a6df4d7816fa61dba0304e0f87354d820b970
Shifu <kris.deniso@mail.ru>
2021-01-05 15:40:23 +0300

    added adaptive to catalog-page

--- app/js/main.js
+++ app/js/main.js
@@ -67,18 +67,18 @@ flatpickr("#book-check-out", {
 
 // кнопка сортировки
 
+const sorting = document.querySelector('.catalog__sorting');
 const selected = document.querySelector('.catalog__selected');
-const wrapper = document.querySelector('.catalog__sorting-wrapper');
 const options = document.querySelectorAll('.catalog__sorting-item');
 
 selected.addEventListener('click', () => {
-  wrapper.classList.toggle('active');
+  sorting.classList.toggle('active');
 });
 
 options.forEach( i => {
   i.addEventListener('click', () => {
     selected.innerHTML = i.querySelector('.catalog__sorting-label').innerHTML;
-    wrapper.classList.remove('active');
+    sorting.classList.remove('active');
   });
 });
 
